7664a7b5ccbd82df5996e95887992ea28ae75c97
OfIkorodu <tommybreezy257@gmail.com>
2022-12-10 14:59:57 -0800

    Variables if else while

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,51 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
- * main - entry point
+ * main - Entry point
  *
- * Return: alway returns 0
- *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	unsigned char a = '0';
-	int i;
+	int d;
+	char low;
 
-	for (i = 0; i < 10; i++)
-	{
-		putchar(a);
-		a++;
-	}
-	a = '1';
-	for (i = 0; i < 6; i++)
-	{
-		putchar('0' + a);
-		a++;
-	}
-	putchar('\n');
-	return (0);
-#include <stdio.h>
-/**
- *  * main - entry point
- *   *
- *    * Return: alway returns 0
- *     *
- *      */
-int main(void)
-{
-	unsigned char a = '0';
-	int i;
+	for (d = '0'; d <= '9'; d++)
+		putchar(d);
+	for (low = 'a'; low <= 'f'; low++)
+		putchar(low);
+		putchar('\n');
 
-	for (i = 0; i < 10; i++)
-	{
-			putchar(a);
-			a++;
-	}
-	a = '1';
-	for (i = 0; i < 6; i++)
-	{
-			putchar('0' + a);
-			a++;
-	}
-	putchar('\n');
 	return (0);
-}}
+}
