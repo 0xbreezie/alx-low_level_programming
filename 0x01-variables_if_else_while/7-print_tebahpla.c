cc02a89a6102953a8e2d555eeb056d438445d8c2
OfIkorodu <tommybreezy257@gmail.com>
2022-12-28 18:32:07 -0800

    variables if else while

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -8,16 +8,14 @@
  */
 int main(void)
 {
-    char letter;
-
-    letter = 'z';
-    while (letter >= 'a')
-    {
-        putchar(letter);
-        letter--;
-    }
-    putchar('\n');
-
-    return (0);
+char letter;
+letter = 'z';
+while (letter >= 'a')
+{
+putchar(letter);
+letter--;
+}
+putchar('\n');
+return (0);
 }
 
