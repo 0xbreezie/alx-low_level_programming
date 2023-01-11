47e17e021eaeeffa7c8f8001b4709cddfc93fb46
OfIkorodu <tommybreezy257@gmail.com>
2023-01-10 16:49:33 -0800

    Static libraries

--- 0x09-static_libraries/4-strpbrk.c
+++ 0x09-static_libraries/4-strpbrk.c
@@ -1,26 +1,28 @@
 #include "main.h"
 
 /**
- * _strpbrk - Searches a string for any of a set of bytes.
- * @s: The string to be searched.
- * @accept: The set of bytes to be searched for.
- *
- * Return: If a set is matched - a pointer to the matched byte.
- * If no set is matched - NULL.
+ * _strpbrk - searches a string for any of a set of bytes.
+ * @s: string to be scanned
+ * @accept: string containing characters to match
+ * Description: This function finds the first character in the string s
+ * that matches any character specified in accept.
+ * This does not include terminating null-characters.
+ * Return: pointer to the byte in s that matches one of the bytes in accept
  */
 char *_strpbrk(char *s, char *accept)
 {
-	int index;
+	int i, j;
 
-	while (*s)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (index = 0; accept[index]; index++)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if (*s == accept[index])
-				return (s);
+			if (s[i] == accept[j])
+			{
+				return (s + i);
+			}
 		}
-
-		s++;
 	}
-	return ('\0');
+
+	return (0);
 }
