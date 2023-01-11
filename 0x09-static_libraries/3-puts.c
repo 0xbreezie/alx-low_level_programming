0fb36f6c08ff42b5e098e572732112fcccef551c
OfIkorodu <tommybreezy257@gmail.com>
2023-01-10 17:12:08 -0800

    static libraries

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -2,16 +2,15 @@
 
 /**
  * _puts - prints a string to stdout
- * @s: string to be printed
- *
- * Return: void
+ * @str: pointer to the string to print
  */
-void _puts(char *s)
+void _puts(char *str)
 {
-	while (*s)
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		_putchar(*s);
-		s++;
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
